2c86f4b16193928c4f17a176b58ea518a772d0db
Russell Templet <russelltemplet@Russells-Mac-mini.local>
2026-07-11 16:49:39 -0700

    fixed deploy issues CNAME

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ export default function App() {
   return (
     <ThemeProvider theme={globalTheme}>
       <CssBaseline />
-      <BrowserRouter basename="/tempurawaves-web">
+      <BrowserRouter>
         <Routes>
           <Route element={<AppLayout />}>
             <Route path="/" element={<HomePage />} />
